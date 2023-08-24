b4763af5043db235401e5b860d32201817202520
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2023-08-24 17:23:43 +0200

    feat(webapps/docker): add gefiproj db backups

---
+++ webapps-srv/home/admin/docker/gefiproj/postgres/backups/backup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Encoding : UTF-8
+# Script to backup Gefiproj database
+
+pg_dump -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" > /backups/$(date +"%Y-%m-%d")_gefiproj_${POSTGRES_DB}.dump
+find  /backups/ -type f -mtime +7 -name '*.dump' -execdir rm -- '{}' \;
