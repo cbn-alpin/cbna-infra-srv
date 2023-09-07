a4fb8492e8c895ecfc82ccb2c5efb61aa37aa242
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2023-09-07 09:12:33 +0200

    feat(webapps/docker/gefiproj): rename built image, use an independent Pgadmin

--- webapps-srv/home/admin/docker/gefiproj/postgres/backups/backup.sh
+++ webapps-srv/home/admin/docker/gefiproj/postgres/backups/backup.sh
@@ -3,4 +3,4 @@
 # Script to backup Gefiproj database
 
 pg_dump -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" > /backups/$(date +"%Y-%m-%d")_gefiproj_${POSTGRES_DB}.dump
-find  /backups/ -type f -mtime +7 -name '*.dump' -execdir rm -- '{}' \;
+find  /backups/ -type f -mtime +4 -name '*.dump' -execdir rm -- '{}' \;
