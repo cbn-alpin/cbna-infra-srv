daaac8ff788061f284a5684ee9ba68e6543f22e8
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2023-10-06 15:15:17 +0200

    feat(webapps/docker/wordpress): improve cron backup.sh script

--- webapps-srv/home/admin/docker/cms-cbna-wordpress/cron/build/backup.sh
+++ webapps-srv/home/admin/docker/cms-cbna-wordpress/cron/build/backup.sh
@@ -7,14 +7,11 @@ BKP_DIR="/backups"
 
 mariadb-dump --host cms-cbna-mariadb --port 3306 \
 	--user root --password=${MARIADB_ROOT_PASSWORD} \
-	--all-databases > ${BKP_DIR}/${TODAY}_cms-cbna_wordpress.dump.sql
+	${WORDPRESS_DB_NAME} > ${BKP_DIR}/${TODAY}_cms-cbna_wordpress.dump.sql
 
-tar -jcvf "${BKP_DIR}/${TODAY}_cms-cbna_wp-content.backup.tar.bz2" /var/www/html/wp-content
+cd /var/www/html/
+tar -jcvf "${BKP_DIR}/${TODAY}_cms-cbna_wp-content.backup.tar.bz2" ./wp-content
 
 chown -R ${HOST_USER_ID}:${HOST_GROUP_ID} "${BKP_DIR}"
 
-find "${BKP_DIR}/" -type f -mtime +1 -regex '.*\.(backup\.tar\.bz2|dump\.sql)$' -exec rm {} \;
-
-for filename in "${BKP_DIR}/*" ; do
-	echo $filename
-done;
+find "${BKP_DIR}/" -type f -mtime +1 -regextype egrep -regex '.*\.(backup\.tar\.bz2|dump\.sql)$' -exec rm {} \;
