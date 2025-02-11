682448f81e125fef8025ca46a9cc77122de060a1
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2025-02-11 16:53:54 +0100

    fix(webapps/cms-cbna-wordpress-test): improve healthcheck, fix backup deleting files

--- webapps-srv/home/admin/docker/cms-cbna-wordpress-test/cron/build/backup.sh
+++ webapps-srv/home/admin/docker/cms-cbna-wordpress-test/cron/build/backup.sh
@@ -7,11 +7,14 @@ BKP_DIR="/backups"
 
 mariadb-dump --host cms-cbna-test-mariadb --port 3306 \
 	--user root --password=${MARIADB_ROOT_PASSWORD} \
-	${WORDPRESS_DB_NAME} > ${BKP_DIR}/${TODAY}_cms-cbna_wordpress.dump.sql
+	--all-databases > ${BKP_DIR}/${TODAY}_cms-cbna_wordpress.dump.sql
 
-cd /var/www/html/
-tar -jcvf "${BKP_DIR}/${TODAY}_cms-cbna_wp-content.backup.tar.bz2" ./wp-content
+tar -jcvf "${BKP_DIR}/${TODAY}_cms-cbna_wp-content.backup.tar.bz2" /var/www/html/wp-content
 
 chown -R ${HOST_USER_ID}:${HOST_GROUP_ID} "${BKP_DIR}"
 
-find "${BKP_DIR}/" -type f -mtime +1 -regex '.*\.(backup\.tar\.bz2|dump\.sql)$' -exec rm {} \;
+find "${BKP_DIR}/" -type f -mtime +1 -regextype egrep -regex '.*\.(backup\.tar\.bz2|dump\.sql)$' -exec rm {} \;
+
+for filename in "${BKP_DIR}/*" ; do
+	echo $filename
+done;
